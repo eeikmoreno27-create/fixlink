3a799e19b51b93d3b9ca58db549608746ce825fb
eeikmoreno27-create <eeikmoreno27@gmail.com>
2025-11-10 00:43:03 -0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
 // firebase.js - configuración integrada con tu proyecto
 export const ADMIN_CODE = 'erik2025';
 
-const firebaseConfig = {
-  "apiKey": "AIzaSyCDI8TKyzuOMSkbzygvxCnGZdSe9hnrGpA",
-  "authDomain": "fixlink-db.firebaseapp.com",
-  "projectId": "fixlink-db",
-  "storageBucket": "fixlink-db.firebasestorage.app",
-  "messagingSenderId": "248111844721",
-  "appId": "1:248111844721:web:8c7645a91e7ad492f63ad0",
-  "measurementId": "G-RNJFL2L3MF"
-};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyCDI8TKyzuOMSkbzygvxCnGZdSe9hnrGpA",
+  authDomain: "fixlink-db.firebaseapp.com",
+  projectId: "fixlink-db",
+  storageBucket: "fixlink-db.firebasestorage.app",
+  messagingSenderId: "248111844721",
+  appId: "1:248111844721:web:8c7645a91e7ad492f63ad0",
+  measurementId: "G-RNJFL2L3MF"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
